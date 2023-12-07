187354f9a235a1222cdec44ff01b186f056b92f5
Sandra Soinska <sandra.soinska@gmail.com>
2023-12-07 14:36:00 +0100

    Replacing secret key with github secrets

--- login_form/__init__.py
+++ login_form/__init__.py
@@ -5,7 +5,8 @@ def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
-        SECRET_KEY='K%OvdP/-5UXS{553J2{#n*Ii03rc>',
+        SECRET_KEY= ${{ secrets.SECRET_KEY }},
+
         DATABASE=os.path.join(app.instance_path, 'login_form.sqlite'),
     )
 
